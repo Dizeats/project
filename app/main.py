dc2b0795eae96c9bb986bcdb84c2ef7f1c680adc
romagav <rom.gavlyasek9@gmail.com>
2024-04-10 22:28:53 +0700

    extended base

--- app/main.py
+++ app/main.py
@@ -3,11 +3,20 @@ from flask import Flask
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
-db_session.global_init("db/users.db")
 
 
 def main():
-    app.run()
+    db_session.global_init("db/users.db")
+
+    @app.route('/registration')
+    def registration():
+        return 'test'
+
+    @app.route('/homepage')
+    def homepage():
+        return 'nothing'
+
+    app.run(port=8080, host='127.0.0.1')
 
 
 if __name__ == '__main__':
