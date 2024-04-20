a91c7b9ca550df786b0f79e6691a129dcf494b15
romagav <rom.gavlyasek9@gmail.com>
2024-04-20 22:07:48 +0700

    prod second version

--- app/main.py
+++ app/main.py
@@ -44,16 +44,19 @@ def main():
 
     @app.route('/signin', methods=['GET', 'POST'])
     def signin():
-        form = SigninForm()
-        if form.validate_on_submit():
-            db_sess = db_session.create_session()
-            user = db_sess.query(User).filter(User.email == form.email.data).first()
-            if user and user.check_password(form.password.data):
-                login_user(user, remember=form.remember_me.data)
-                return redirect("/homepage")
-            return render_template('signin_' + lang + '.html', message="Неправильный логин или пароль", form=form,
-                                   title='Вход')
-        return render_template('signin_' + lang + '.html', form=form, title='Вход')
+        if not request.cookies.get('remember_token'):
+            form = SigninForm()
+            if form.validate_on_submit():
+                db_sess = db_session.create_session()
+                user = db_sess.query(User).filter(User.email == form.email.data).first()
+                if user and user.check_password(form.password.data):
+                    login_user(user, remember=form.remember_me.data)
+                    return redirect("/homepage")
+                return render_template('signin_' + lang + '.html', message="Неправильный логин или пароль", form=form,
+                                       title='Вход')
+            return render_template('signin_' + lang + '.html', form=form, title='Вход')
+        else:
+            return redirect('/homepage')
 
     @app.route('/registration', methods=['GET', 'POST'])
     def registration():
@@ -78,32 +81,40 @@ def main():
 
     @app.route('/homepage', methods=['GET', 'POST'])
     def homepage():
-        if request.cookies.get('remember_token') == '':
+        if not request.cookies.get('remember_token'):
             return redirect('/')
         bas = connect('db/users.db')
         cur = bas.cursor()
         id_of_user = cur.execute('''SELECT id FROM users WHERE email = ?''', (current_user.email,)).fetchall()
-        products = cur.execute('''SELECT * FROM products WHERE user_id = ?''', (id_of_user[0][0], )).fetchall()
+        products = cur.execute('''SELECT * FROM products WHERE user_id = ?''', (id_of_user[0][0],)).fetchall()
+        counter_of_products = len(products)
         form = HomepageForm()
         if form.add.data:
             added_product = form.name_added_product.data
-            check_product = cur.execute('''SELECT id FROM products WHERE name = ?''', (added_product, )).fetchall()
-            if check_product:
-                er = 'Такой продукт уже есть'
-                return render_template('homepage_' + lang + '.html', products=products, title='Главная', form=form, er=er)
+            check_product = cur.execute('''SELECT user_id FROM products WHERE name = ?''', (added_product,)).fetchall()
+            print(check_product)
+            print(id_of_user)
+            if check_product and check_product[0][0] == id_of_user[0][0]:
+                error = 'Такой продукт уже есть'
+                return render_template('homepage_' + lang + '.html', products=products, title='Главная', form=form,
+                                       error=error, counter=counter_of_products)
             cur.execute('''INSERT INTO products(user_id, name) VALUES(?, ?)''', (id_of_user[0][0], added_product))
             bas.commit()
-            redirect('/homepage')
+            form.name_added_product.data = ''
+            return redirect('/homepage')
         if form.delete.data:
             deleted_product = form.name_deleted_product.data
-            id_of_product = cur.execute('''SELECT id FROM products WHERE name = ?''', (deleted_product, )).fetchall()
+            id_of_product = cur.execute('''SELECT id FROM products WHERE name = ?''', (deleted_product,)).fetchall()
             if not id_of_product:
-                er = 'Такого продукта нет'
-                return render_template('homepage_' + lang + '.html', products=products, title='Главная', form=form, er=er)
-            cur.execute('''DELETE FROM products WHERE id = ?''', (id_of_product[0][0], ))
+                error = 'Такого продукта нет'
+                return render_template('homepage_' + lang + '.html', products=products, title='Главная', form=form,
+                                       error=error, counter=counter_of_products)
+            cur.execute('''DELETE FROM products WHERE id = ?''', (id_of_product[0][0],))
             bas.commit()
-            redirect('/homepage')
-        return render_template('homepage_' + lang + '.html', products=products, title='Главная', form=form, er='')
+            form.name_deleted_product.data = ''
+            return redirect('/homepage')
+        return render_template('homepage_' + lang + '.html', products=products, title='Главная', form=form, error='',
+                               counter=counter_of_products)
 
     @app.route('/logout')
     def logout():
